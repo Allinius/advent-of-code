db541f3d137e78e49575ccbba13d5d548e302dd6
Allinius <michal.durovec@gmail.com>
2021-12-01 14:30:03 +0100

    add day 1 solution

---
+++ 01/app.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const smallInput = fs.readFileSync('input-small.txt', 'utf-8').split('\n').map(s => parseInt(s));
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n').map(s => parseInt(s));
+
+
+const partOne = (depths) => {
+    let increaseCount = 0;
+    let previousDepth;
+    depths.forEach(depth => {
+        if (previousDepth && depth > previousDepth) {
+            increaseCount++;
+        }
+        previousDepth = depth;
+    });
+
+    return increaseCount
+}
+
+const partTwo = (depths) => {
+    const windows = [];
+
+    for (let i = 0; i < depths.length - 2; i++) {
+        windows.push(depths[i] + depths[i + 1] + depths[i + 2])
+    }
+    
+    return partOne(windows);
+}
+
+console.log(partOne(input));
+
+console.log(partTwo(input));
