6240c84a35ee784f3c619e7d0b48cdd24ce47d35
Allinius <michal.durovec@gmail.com>
2021-12-08 11:01:12 +0100

    add day 7 solution

---
+++ 07/app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8').split(',').map(n => parseInt(n)).sort((a,b)=>a-b);
+
+const partOne = (positions) => {
+    const position = positions[positions.length/2];
+    return positions.reduce((prev, curr) => prev + (Math.abs(position - curr)), 0);
+}
+
+const partTwo = (positions) => {
+    // idk why it's floor not round
+    const position = Math.floor(positions.reduce((prev, curr) => prev + curr, 0) / positions.length);
+    console.log(position);
+    return positions.reduce((prev, curr) => {
+        const diff = Math.abs(position - curr);
+        return prev + (diff * (diff + 1)) / 2
+    }, 0);
+}
+
+console.log(partOne(input));
+console.log(partTwo(input));
