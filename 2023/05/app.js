b3b54ed31ad65edc13b0fb6736dae87ee7ac8dad
Allinius <michal.durovec@gmail.com>
2023-12-05 14:17:53 +0100

    simplify 2023/05 input parsing

--- 2023/05/app.js
+++ 2023/05/app.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 const assert = require('assert');
-const {
-    intervalUnion,
-    intervalIntersection,
-} = require('../../common/number-utils');
 
 const parseInput = (fileName) => {
     const clusters = fs
@@ -16,19 +12,20 @@ const parseInput = (fileName) => {
         .slice(1)
         .map((n) => Number.parseInt(n));
     const maps = clusters.slice(1).map((cluster) => {
-        const name = cluster[0];
-        const rules = cluster
+        return (rules = cluster
             .slice(1)
             .map((line) => {
                 const parts = line.split(' ');
                 return {
-                    destStart: Number.parseInt(parts[0]),
-                    sourceStart: Number.parseInt(parts[1]),
-                    length: Number.parseInt(parts[2]),
+                    start: Number.parseInt(parts[1]),
+                    end:
+                        Number.parseInt(parts[1]) +
+                        Number.parseInt(parts[2]) -
+                        1,
+                    diff: Number.parseInt(parts[0]) - Number.parseInt(parts[1]),
                 };
             })
-            .sort((a, b) => a.sourceStart - b.sourceStart);
-        return { name, rules };
+            .sort((a, b) => a.sourceStart - b.sourceStart));
     });
     return {
         seeds,
@@ -42,7 +39,7 @@ const input = parseInput('input.txt');
 const partOne = (input) => {
     return input.seeds.reduce((min, seed) => {
         const location = input.maps.reduce(
-            (prev, map) => getNext(prev, map.rules),
+            (prev, rules) => getNext(prev, rules),
             seed
         );
         if (location < min) {
@@ -54,12 +51,10 @@ const partOne = (input) => {
 
 const getNext = (source, rules) => {
     const validRule = rules.find(
-        (rule) =>
-            rule.sourceStart <= source &&
-            rule.sourceStart + rule.length > source
+        (rule) => rule.start <= source && rule.end >= source
     );
     if (validRule) {
-        return validRule.destStart + (source - validRule.sourceStart);
+        return source + validRule.diff;
     }
     return source;
 };
@@ -86,25 +81,15 @@ partTwoBruteForce = (input) => {
 };
 
 partTwo = (input) => {
-    let min = Number.MAX_SAFE_INTEGER;
-
-    const maps = input.maps.map((map) =>
-        map.rules.map((rule) => ({
-            start: rule.sourceStart,
-            end: rule.sourceStart + rule.length - 1,
-            diff: rule.destStart - rule.sourceStart,
-        }))
+    let currRanges = input.seeds.reduce(
+        (res, curr, i) =>
+            i % 2 === 0
+                ? [...res, { start: curr, end: curr + input.seeds[i + 1] - 1 }]
+                : res,
+        []
     );
-    let currRanges = [];
 
-    for (let i = 0; i < input.seeds.length; i += 2) {
-        currRanges.push({
-            start: input.seeds[i],
-            end: input.seeds[i] + input.seeds[i + 1] - 1,
-        });
-    }
-
-    maps.forEach((rules) => {
+    input.maps.forEach((rules) => {
         const newRanges = [];
         currRanges.forEach((sourceRange) => {
             newRanges.push(...getNextRanges(sourceRange, rules));
